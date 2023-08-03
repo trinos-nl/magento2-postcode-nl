c98d992b3dc5225cc5bba139a86be2e740f51118
Thijs de Witt <thijs@trinos.nl>
2023-08-03 16:11:35 +0200

    Fix billing address fields order

--- src/Plugin/SortFormFieldsMapping.php
+++ src/Plugin/SortFormFieldsMapping.php
@@ -20,4 +20,19 @@ class SortFormFieldsMapping
         ksort($result);
         return $result;
     }
+
+    /**
+     * The reason for sorting is so that street.0 is added before street.1, etc.
+     * This way we force street.0 to always be the ancestor
+     *
+     * @param Checkout $subject
+     * @param array $result
+     * @param string $by
+     * @return array
+     */
+    public function afterGetBillingEavAttributeFormFieldsMapping(Checkout $subject, array $result, string $by = 'attribute_code'): array
+    {
+        ksort($result);
+        return $result;
+    }
 }
